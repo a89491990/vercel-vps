dceb65cd24f4125cfb5d895b0cab2ccc957728bd
a89491990 <a89491990@gmail.com>
2025-11-05 12:23:48 +0600

    Delete api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    status: 'success',
-    message: '🚀 Server running on Vercel!',
-    time: new Date().toISOString(),
-  });
-}
